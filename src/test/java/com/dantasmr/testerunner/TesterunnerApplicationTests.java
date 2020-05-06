a7643d1f942a7b4436369b0002daaf3e579c1a26
Marcelo <dantasmr@yahoo.com.br>
2020-05-06 08:05:37 -0300

    first commit

---
+++ src/test/java/com/dantasmr/testerunner/TesterunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dantasmr.testerunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesterunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
